060a260888fafc436f75f1a30d01e06b17526479
Carlos Piña <carlos.p.m.2293@gmail.com>
2022-12-21 09:02:28 +0100

    ADD - Project structure

---
+++ app/src/main/java/com/pineappleC/pokedex/PokedexApp.kt
@@ -0,0 +1,10 @@
+package com.pineappleC.pokedex
+
+import android.app.Application
+
+/* 
+    Created by Carlos Piña on 20/12/22.
+    Copyright (c) 2022 ElConfidencial. All rights reserved.
+*/
+
+class PokedexApp : Application()
