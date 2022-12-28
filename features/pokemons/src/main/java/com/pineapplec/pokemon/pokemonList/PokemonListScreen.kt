0c5a8a58219c1ab3f0da503218e1502dd5407f66
Carlos Piña <carlos.p.m.2293@gmail.com>
2022-12-28 01:46:56 +0100

    ADD - Pokemon list enter animation

--- features/pokemons/src/main/java/com/pineapplec/pokemon/pokemonList/PokemonListScreen.kt
+++ features/pokemons/src/main/java/com/pineapplec/pokemon/pokemonList/PokemonListScreen.kt
@@ -1,5 +1,9 @@
 package com.pineapplec.pokemon.pokemonList
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,11 +20,16 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
@@ -65,16 +74,36 @@ fun PokemonListScreen(
         }
     }
 
-    (pokemonUiState as? PokemonListUiState.Result)?.let { result ->
-        LazyVerticalGrid(
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            columns = GridCells.Fixed(2),
-        ) {
-            items(result.pokemonList.size, key = { result.pokemonList[it].id }) { index ->
-                PokemonItem(pokemon = result.pokemonList[index], navController = navController)
+    var listVisibility by remember { mutableStateOf(false) }
+    when (pokemonUiState) {
+        PokemonListUiState.Loading -> {
+            Box(modifier = Modifier.fillMaxSize()) {
+                CircularProgressIndicator(Modifier.align(Alignment.Center))
             }
+            listVisibility = false
+        }
+        is PokemonListUiState.Result -> {
+            val result = pokemonUiState as PokemonListUiState.Result
+            AnimatedVisibility(
+                visible = listVisibility,
+                enter = fadeIn(animationSpec = tween(1_000)),
+                exit = fadeOut(animationSpec = tween(1_000))
+            ) {
+                LazyVerticalGrid(
+                    contentPadding = PaddingValues(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    columns = GridCells.Fixed(2),
+                ) {
+                    items(result.pokemonList.size, key = { result.pokemonList[it].id }) { index ->
+                        PokemonItem(
+                            pokemon = result.pokemonList[index],
+                            navController = navController
+                        )
+                    }
+                }
+            }
+            listVisibility = true
         }
     }
 }
@@ -82,7 +111,7 @@ fun PokemonListScreen(
 @Composable
 fun PokemonItem(
     pokemon: PokemonItem,
-    navController: NavController
+    navController: NavController,
 ) {
     Card(
         elevation = 4.dp,
